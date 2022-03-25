6ec8d7726b8bfd62a2d2c4bec1ace2fc17e754db
Mark Perov <mark.perov@stud.fh-campuswien.ac.at>
2022-03-25 13:32:52 +0100

    First Test written

---
+++ src/main/java/NumberOfMasses.java
@@ -0,0 +1,11 @@
+public class NumberOfMasses {
+    public static int countNumberOfMasses(int[] masses) {
+        int count = 0;
+
+        for (int mass :
+                masses) {
+            count++;
+        }
+        return count;
+    }
+}
